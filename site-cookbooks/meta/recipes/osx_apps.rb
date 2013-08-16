787935e3f1882e4a263f38693ad16e8aba03743e
Aiyesha Ma <a.ma@modcloth.com>
2013-08-16 15:22:48 -0700

    Remove unused osx* and pivotal* recipes.

--- site-cookbooks/meta/recipes/osx_apps.rb
+++
@@ -1,7 +0,0 @@
-include_recipe "sprout-osx-apps::ack"
-include_recipe "sprout-osx-apps::caffeine"
-include_recipe "sprout-osx-apps::flux"
-include_recipe "sprout-osx-apps::freeruler"
-include_recipe "pivotal_workstation::omnigraffle"
-
-
--- site-cookbooks/meta/recipes/osx_settings.rb
+++
@@ -1,4 +0,0 @@
-include_recipe "sprout-osx-settings::defaults_fast_key_repeat_rate"
-include_recipe "sprout-osx-settings::osx_disable_natural_scrolling"
-include_recipe "sprout-osx-settings::set_finder_show_user_home_in_sidebar"
-include_recipe "sprout-osx-settings::dock_preferences"
