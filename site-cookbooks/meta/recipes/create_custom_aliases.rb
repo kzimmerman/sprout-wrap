4598176be76d14a52b9567829ececa05bbec7005
Aiyesha Ma <a.ma@modcloth.com>
2013-08-16 15:20:30 -0700

    change custom aliases to be a template (with the correct workspace directory variable)

--- site-cookbooks/meta/recipes/create_custom_aliases.rb
+++ site-cookbooks/meta/recipes/create_custom_aliases.rb
@@ -1,5 +1,5 @@
-include_recipe "pivotal_workstation::bash_it"
-cookbook_file "/Users/#{node['current_user']}/.bash_it/aliases/custom.aliases.bash" do
+include_recipe "sprout-osx-base::bash_it"
+template "/Users/#{node['current_user']}/.bash_it/aliases/custom.aliases.bash" do
   source 'custom.aliases.bash'
   owner node['current_user']
   mode 0644
